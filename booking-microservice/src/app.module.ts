73c890058633ed1516e8b4e212b54a549574f7b1
Dduquejh <johandanielduquehernandez@gmail.com>
2024-11-04 22:42:30 -0500

    Config NATS and database

--- booking-microservice/src/app.module.ts
+++ booking-microservice/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [BookingModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+      entities: [],
+    }),
+    BookingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- booking-microservice/src/booking/entities/booking.entity.ts
+++ booking-microservice/src/booking/entities/booking.entity.ts
@@ -1,2 +1,1 @@
-export class Booking {
-}
+export class Booking {}
